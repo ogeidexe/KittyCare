ec265c3c0598fc0fa184fb26fcdc4ea2c7032e3a
Diego Teixeira <ogeidexe@gmail.com>
2019-04-05 19:30:14 -0300

    frist

---
+++ src/TesteServlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+
+public class TesteServlet extends HttpServlet   {
+	public void doGet(){
+		
+	}
+	public void doPost(){
+		
+	}
+	
+}
