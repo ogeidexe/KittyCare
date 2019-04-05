263cf9d1383550f7f9d379fb6e97761af61b35de
Diego Teixeira <ogeidexe@gmail.com>
2019-04-05 20:47:40 -0300

    Formulario dados dono

--- src/TesteServlet.java
+++ src/TesteServlet.java
@@ -3,10 +3,15 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.sql.*;
 
+@SuppressWarnings("serial")
 public class TesteServlet extends HttpServlet   {
-	public void doGet(){
-		
-	}
+	public void doGet(HttpServletRequest request, HttpServletResponse
+	response) throws
+	    ServletException, IOException {
+		response.setContentType("text/html");
+	      PrintWriter out = response.getWriter();
+	      
+	  }
 	public void doPost(){
 		
 	}
